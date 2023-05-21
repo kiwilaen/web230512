923b0623d99479711b60f491718815983ec9edd1
kiwilaen <97778141+kiwilaen@users.noreply.github.com>
2023-05-21 15:13:11 +0800

    Add files via upload

--- static/js/login.js
+++ static/js/login.js
@@ -5,21 +5,45 @@ const signInButton = document.getElementById('signIn')
 const container = document.getElementById('container')
 const studentSignIn = document.getElementById('studentSignIn')
 
+//在页面加载的时候调用getCaptchaBase64接口
+function getCaptchaBase64() {
+    const img = document.getElementById('img')
+    ajaxGet("/getCaptchaBase64",formatter({
+        img : img.value
+    })
+        .then((res) => {
+            console.log(res.url)
+            res = JSON.parse(res.url)
+            if (res.success) {
+                img.src = res.url
+                container.appendChild(img)
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    )
+}
+
+
+window.onload = function() {
+    getCaptchaBase64();
+}
+
 
 
 studentSignIn.addEventListener('click', () => {
     const accountNum = document.getElementById('accountNum')
     const password = document.getElementById('password')
-    window.location.href = '../static/news.html'
-    ajaxGet("/test/student/studentLogin", formatter({
+    window.location.href = './main/news.html'
+    ajaxGet("/user/login", formatter({
         accountNum: accountNum.value,
-            password: password.value
-        }))
+        password: password.value,
+        })
         .then((res) => {
             console.log(res)
             res = JSON.parse(res)
             if (res.success) {
-                window.location.href = '../news.html'
             } else {
                 alert('账号或密码错误!请重新输入')
                 cleaner(accountNum, password)
@@ -27,16 +51,35 @@ studentSignIn.addEventListener('click', () => {
         })
         .catch((err) => {
             console.log(err)
+        }))
+    ajaxGet("/checkCaptcha", formatter({
+        code : code.value
+    }))
+        .then((res) => {
+            console.log(res)
+            res = JSON.parse(res)
+            if (res.success) {
+                window.location.href = './main/news.html'
+            } else {
+                alert('验证码错误!请重新输入')
+                cleaner(code)
+            }
         })
+        .catch((err) => {
+            console.log(err)
+        })
+
+
 })
 
 
 var signInSuccess = function() {
-    window.location.href = '../news.html'
+    window.location.href = './main/news.html'
 }
 
 var signInFail = function(n, p) {
     alert('账号或密码错误!请重新输入')
     n.value = ''
     p.value = ''
+    cleaner(n, p);
 }
